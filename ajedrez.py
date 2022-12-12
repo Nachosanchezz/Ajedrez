4847153ae17fc59b784b351b63d7089f3604f7e3
Nacho <isanccal@myuax.com>
2022-12-12 18:46:52 +0100

    Cambio 3

--- ajedrez.py
+++ ajedrez.py
@@ -8,4 +8,36 @@ def partida_ajedrez(numero_fichero):
         f.close()
         return tablero
     movimientos = 0
-    
+    while True:
+        print('Turno del jugador blanco')
+        print(tablero)
+        movimiento = input('Introduzca el movimiento: ')
+        if movimiento == '0-0':
+            tablero[7][4] = '\t'
+            tablero[7][6] = '♔'
+            tablero[7][7] = '\t'
+            tablero[7][5] = '♖'
+        elif movimiento == '0-0-0':
+            tablero[7][4] = '\t'
+            tablero[7][2] = '♔'
+            tablero[7][0] = '\t'
+            tablero[7][3] = '♖'
+        else:
+            tablero[int(movimiento[1])-1][ord(movimiento[0])-97] = '\t'
+            tablero[int(movimiento[3])-1][ord(movimiento[2])-97] = '♔'
+        movimientos += 1
+        print('Turno del jugador negro')
+        print(tablero)
+        movimiento = input('Introduzca el movimiento: ')
+        if movimiento == '0-0':
+            tablero[0][4] = '\t'
+            tablero[0][6] = '♚'
+            tablero[0][7] = '\t'
+            tablero[0][5] = '♜'
+        elif movimiento == '0-0-0':
+            tablero[0][4] = '\t'
+            tablero[0][2] = '♚'
+            tablero[0][0] = '\t'
+            tablero[0][3] = '♜'
+        else:
+            
