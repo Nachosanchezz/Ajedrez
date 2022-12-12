14181121a0c8d2880f543a60473f76a5eaf3d677
Nacho <isanccal@myuax.com>
2022-12-12 19:01:58 +0100

    Cambio 4

--- ajedrez.py
+++ ajedrez.py
@@ -1,18 +1,15 @@
-def partida_ajedrez(numero_fichero):
+def partida_ajedrez(nombre_fichero):
     tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
     tablero = []
-    for i in range('\n' in tablero_inicial'):
-        tablero.append(tablero_inicial.split('\t')[8*i:8*(i+1)])
-        f = open(nombre_fichero, 'w')
-        f.write(tablero)
-        f.close()
-        return tablero
+    for i in range(8):
+        tablero.append(tablero_inicial.split('\t'))
+    print(tablero)
     movimientos = 0
     while True:
         print('Turno del jugador blanco')
         print(tablero)
-        movimiento = input('Introduzca el movimiento: ')
-        if movimiento == '0-0':
+        movimiento = input('Quiere hacer otro movimiento: ')
+        if movimiento != 'Si':
             tablero[7][4] = '\t'
             tablero[7][6] = '♔'
             tablero[7][7] = '\t'
@@ -39,5 +36,24 @@ def partida_ajedrez(numero_fichero):
             tablero[0][2] = '♚'
             tablero[0][0] = '\t'
             tablero[0][3] = '♜'
-        else:
-            
+        else:   
+            tablero[int(movimiento[1])-1][ord(movimiento[0])-97] = '\t'
+            tablero[int(movimiento[3])-1][ord(movimiento[2])-97] = '♚'
+        movimientos += 1
+        f = open(nombre_fichero, 'w')
+        f.write(tablero)
+        f.close()
+        if movimientos == 100:
+            print('Empate')
+            break
+        elif tablero[0][4] == '\t':
+            print('Ganan las negras')
+            break
+        elif tablero[7][4] == '\t':
+            print('Ganan las blancas')
+            break
+
+partida_ajedrez('partida_ajedrez.txt')
+
+
+
