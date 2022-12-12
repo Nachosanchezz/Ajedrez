ba2f0e88041deab7ad5b6fdc3929ecbc33cdb9cc
Nacho <isanccal@myuax.com>
2022-12-12 19:32:38 +0100

    Cambio 8

--- ajedrez2.py
+++ ajedrez2.py
@@ -1,7 +1,8 @@
 def tablero(nombre_fichero, n):
-    f = open(nombre_fichero, 'w')
+    f = open(nombre_fichero, 'r')
     tableros = f.read().split('\n')
     for i in tableros[n*8:(n+1)*8]: 
        print(i)
     return
 tablero('partida-ajedrez.txt', 2)
+
